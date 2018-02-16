e53f0f4b5a286e2b9da89f3887ed6246bf69fc11
Quentin Hausser <hausser.quentin@gmail.com>
2018-02-16 22:07:29 +0100

    Updated throw

--- dist/library.js
+++ dist/library.js
@@ -14,9 +14,20 @@ var _extends4 = _interopRequireDefault(_extends3);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var combineWrappers = function combineWrappers(wrappers) {
+var combineWrappers = function combineWrappers(wrappers, _ref, _ref2) {
+  var _ref$reqMiddlewares = _ref.reqMiddlewares,
+      reqMiddlewares = _ref$reqMiddlewares === undefined ? [] : _ref$reqMiddlewares,
+      _ref$reqMiddlewarePar = _ref.reqMiddlewareParams,
+      reqMiddlewareParams = _ref$reqMiddlewarePar === undefined ? null : _ref$reqMiddlewarePar;
+  var _ref2$errMiddlewares = _ref2.errMiddlewares,
+      errMiddlewares = _ref2$errMiddlewares === undefined ? [] : _ref2$errMiddlewares,
+      _ref2$errMiddlewaresP = _ref2.errMiddlewaresParams,
+      errMiddlewaresParams = _ref2$errMiddlewaresP === undefined ? null : _ref2$errMiddlewaresP;
+
   var wrapped = {};
   Object.keys(wrappers).forEach(function (key) {
+    wrappers[key].addRequestMiddlewares(reqMiddlewares, reqMiddlewareParams);
+    wrappers[key].addErrorMiddlewares(errMiddlewares, errMiddlewaresParams);
     wrapped = (0, _extends4.default)((0, _defineProperty3.default)({}, key, wrappers[key].routes), wrapped);
   });
   return wrapped;
--- src/library.js
+++ src/library.js
@@ -1,6 +1,10 @@
-const combineWrappers = (wrappers) => {
+const combineWrappers = (wrappers,
+  { reqMiddlewares = [], reqMiddlewareParams = null },
+  { errMiddlewares = [], errMiddlewaresParams = null }) => {
   let wrapped = {};
   Object.keys(wrappers).forEach((key) => {
+    wrappers[key].addRequestMiddlewares(reqMiddlewares, reqMiddlewareParams);
+    wrappers[key].addErrorMiddlewares(errMiddlewares, errMiddlewaresParams);
     wrapped = { [key]: wrappers[key].routes, ...wrapped };
   });
   return wrapped;
